edb75ff81ba6f3cf408b46fa0adf854809d9d01c
vusi <baloyi.trev@gmail.com>
2018-06-11 16:19:41 +0200

    started adding html and css

--- js/ShoeDom.js
+++ js/ShoeDom.js
@@ -12,10 +12,6 @@ class shoe {
 
 };
 
-
 let shoesArray = [new shoe('Nike', 'Black', 500, 10, 10), new shoe('Adidas', 'White', 250, 10, 5), new shoe('Nike', 'White', 750, 8, 6), new shoe('Jordan', 'Pink', 999, 4, 6), new shoe('Adidas', 'Pink', 800, 15, 7)];
 
 shoes = ShoeFactory(shoesArray);
-console.log(shoes.getShoes({
-    brand: 'Nike'
-}));
