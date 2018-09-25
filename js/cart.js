cee39e855ec77fa5517118b4cd7074d3cc0c3ee2
vusi <baloyi.trev@gmail.com>
2018-09-25 10:30:54 +0200

    added checkout button

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,10 @@
 let shoesData = JSON.parse(localStorage.getItem('shoesData'));
-
 shoes = ShoeFactory(shoesData);
-
 let cart = JSON.parse(localStorage.getItem('cart'));
-
-if (cart) {
-    console.log('Found some shoes in the cart');
-} else {
-    console.log('this cart is empty');
-}
-
-
-
 let tmpCart = JSON.parse(localStorage.getItem('cart'));
+let checkOutBtn = document.getElementById('checkOutBtn');
 
-// let tmpCart = getTmpShoes();
-
+// =============================DISPLAY THE SHOES IN THE CART ===============================================// 
 
 let displayCart = function (shoesToDisplay) {
     let cartData = {};
@@ -30,18 +19,13 @@ let displayCart = function (shoesToDisplay) {
     var cartHTML = cartTemplate(cartData);
     cartDataElement.innerHTML = cartHTML;
 };
-
-
-// ============================================= REMOVE FROM CART ====================================================== //
+// ================================ REMOVE SHOE FROM CART ====================================================== //
 let removeFromCart = function (item) {
     let tmpCart = JSON.parse(localStorage.getItem('cart'));
     let shoeToRemove = shoes.getShoes({
         id: item
     })[0];
-
-
-    //remove the item from list
-
+    //overwrite the cart with itself excluding the shoe being removed
     try {
         tmpCart = tmpCart.filter(function (shoe) {
             return parseInt(item) !== shoe.id;
@@ -52,79 +36,12 @@ let removeFromCart = function (item) {
         localStorage.setItem('cart', JSON.stringify(tmpCart));
         displayCart(tmpCart);
     }
-
-
-
-
-
-
-
-    // tmpCart = tmpCart.splice
-
-    // console.log(tmpCart);
-    // console.log(shoeToRemove);
-
-    // shoeToAdd[0].qty++;
-    // shoeIndex = tmpCart.indexOf(shoeToRemove);
-    // console.log(shoeIndex);
-
-
-
-
-
-    // tmpCart.push(shoeToAdd[0]);
-    // localStorage.setItem('shoesData', JSON.stringify(shoesData));
-
-
-
-
-
-
-
-    // if (!tmpCart.some(function (shoe) {
-    //         return shoe.id === shoeToAdd[0].id;
-    //     })) {
-    //     if (shoeToAdd[0].qty > 0) {
-
-    //     };
-    // };
 };
 
+// =========================================CHECKOUT==========================================================//
 
-
-
-
-
-// let removeFromCart = function (item) {
-//     let shoeToRemove = shoes.getShoes({
-//         id: item
-//     });
-//     tmpShoes = tmpShoes.filter(function (shoe) {
-//         return shoe.id !== shoeToRemove[0].id
-//     });
-//     setTmpShoes();
-
-//     if (tmpShoes.length > 0) {
-//         displayCart(tmpShoes);
-//     } else {
-//         displayCart('Cart Empty!')
-//     }
-// };
-
-
-// // ==============================the code below will be used for a sliding cart in future===================================//
-
-// // var maxHoldingArrayIndex = holdingArray.length
-// // var shoeElementIndex = 1;
-
-// // function nextShoeElement() {
-// //     shoeElementIndex += 1;
-// //     if (shoeElementIndex > maxHoldingArrayIndex) {
-// //         shoeElementIndex = 0;
-// //     }
-// //     if (tmpShoes[shoeElementIndex]) {
-// //         displayCart([tmpShoes[shoeElementIndex]]);
-// //     } else {
-// //         displayCart('Cart Empty!');
-// //     }
-// // }
+checkOutBtn.addEventListener('click', function checkout() {
+    localStorage.setItem('cart', JSON.stringify([]));
+    displayCart([]);
+    alert('Proceeding to payment');
+});
